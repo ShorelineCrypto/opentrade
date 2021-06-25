add87abdcadca2d8b2471b23613d13a0be65778b
ShorelineCrypto <admin@shorelinecrypto.com>
2021-06-25 16:02:13 -0400

    remove market in api getmarkets24 to avoid confusion

--- server/modules/api/v1.js
+++ server/modules/api/v1.js
@@ -82,7 +82,6 @@ exports.onGetMarkets24 = function(req, res)
        {
         let tmpDict = {
          "name": "",
-         "market": "",
          "bid" : 0,
          "ask" : 0,
          "last" : 0,
@@ -94,7 +93,6 @@ exports.onGetMarkets24 = function(req, res)
         market.BTC_History24(BTC, data2 => {
                console.log(JSON.stringify(data2.result));
                tmpDict["name"] =  utils.stdMarketStr(data2.result.market);
-               tmpDict["market"] =  data2.result.market;
                tmpDict["bid"] = data2.result.Bid;
                tmpDict["ask"] = data2.result.Ask;
                tmpDict["last"] = data2.result.Last;
